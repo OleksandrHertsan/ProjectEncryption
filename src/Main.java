b35106ffd1d5e52a0cddd35d3470f724acfe2c37
Alex <alexgercan88@gmail.com>
2023-02-14 18:29:04 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String original = "Привет, мир";
+        String reversedOrder = "евирпим, тр";
+        
+        List<String> substrings = Encrypt.splitToSubstrings(reversedOrder);
+
+        String resultText = Encrypt.buildEncryptedString(substrings);
+        System.out.println(resultText);
+
+    }
+}
